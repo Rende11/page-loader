c9d94baa2b783a16a6819cf2559fdba7efbce7e6
rende11 <artamonovn@gmail.com>
2017-10-03 01:02:48 +0300

    fix package version

--- src/bin/index.js
+++ src/bin/index.js
@@ -5,15 +5,12 @@ import cli from 'commander';
 import fs from 'fs';
 import loader from '../';
 
-const config = fs.readFileSync('./package.json', 'utf8');
-const { version } = JSON.parse(config);
 
 cli
-  .version(version)
+  .version('0.0.3')
   .arguments('<url>')
   .description('Download data from url')
   .action(url =>
     console.log(loader(url, cli.output)))
   .option('-o, --output [path]', 'Path for saving data')
   .parse(process.argv);
-console.log(process.argv);
