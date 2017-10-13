c29c88ebe87fe57f04b2c6cd72d9c2f166bbf8f7
Nikolay <artamonov.nikolay@e-queo.com>
2017-10-13 19:11:56 +0300

    in progress

--- src/loader.js
+++ src/loader.js
@@ -18,15 +18,13 @@ const errorsList = {
   EEXIST: 'ERROR: File already exists',
   EACCES: 'ERROR: Permission denied',
 };
-export const loadHtml = (url: string, route: string = './') => {
+
+export const loadHtml = async (url: string, route: string = './') => {
   log('Start loading %s', url);
   const filePath = path.join(route, generateHtmlName(url));
-  return axios.get(url)
-    .then(response => response.data)
-    .then((data) => {
-      log('Saving HTML file to %s', filePath);
-      return mz.writeFile(filePath, data, 'utf8');
-    });
+  const response = await axios.get(url)
+  log('Saving HTML file to %s', filePath);
+  return mz.writeFile(filePath, response.data, 'utf8');
 };
 
 const loadRes = (url: string, route: string = './') => {
@@ -40,10 +38,11 @@ const loadRes = (url: string, route: string = './') => {
   const tasks = new Listr([
     {
       title: `Load resourse ${url}`,
-      task: () => axios(options).then((content) => {
+      task: async () => {
+        const content = await axios(options);
         log('Saving resourse %s', name);
         return content.data.pipe(mz.createWriteStream(full));
-      }),
+      },
     },
   ]);
   tasks.run().catch(err => (err));
@@ -54,7 +53,7 @@ const replacer = (filePath, content, url) => {
   return mz.writeFile(filePath, replaceTagsPath(content, generateDirName(url)));
 };
 
-const loader = (url: string, route: string = './') => {
+const loader2 = (url: string, route: string = './') => {
   const dirName = path.join(route, generateDirName(url));
   log('Resourses directory %s', dirName);
   const filePath = path.join(route, generateHtmlName(url));
@@ -80,7 +79,37 @@ const loader = (url: string, route: string = './') => {
       }
       if (error.code) {
         return Promise.reject(errorsList[error.code]);
+     }
+      return Promise.reject(error);
+    });
+};
+
+
+const loader = async (url: string, route: string = './') => {
+  const dirName = path.join(route, generateDirName(url));
+  log('Resourses directory %s', dirName);
+  const filePath = path.join(route, generateHtmlName(url));
+  log('File path - %s', filePath);
+  const html = await loadHtml(url, route);
+  mz.mkdir(dirName)
+  const { host } = new URL(url);
+  const links = getLinks(html, host);
+  log('Founded resourses <- %s', links.length);
+  return Promise.all([...links.map(link => loadRes(link, dirName)),
+        replacer(filePath, content, url)])
+
+    .then(() => {
+      log('Saved on %s', filePath);
+      return filePath;
+    })
+    .catch((error) => {
+      if (error.response) {
+        const responseError = `ERROR: ${error.message}`;
+        return Promise.reject(responseError);
       }
+      if (error.code) {
+        return Promise.reject(errorsList[error.code]);
+     }
       return Promise.reject(error);
     });
 };
